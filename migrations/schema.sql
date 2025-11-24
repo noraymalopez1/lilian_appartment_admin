88df24dad29c222f2b0a542ba6e2739840d97720
aamir <aamirkhan@engineer.com>
2025-11-25 00:53:59 +0500

    chore: add listing schema

---
+++ migrations/schema.sql
@@ -0,0 +1,39 @@
+create table public.listings (
+    uid text primary key,
+
+    title text,
+    category text,
+    tags text,                       -- comma separated string
+
+    description text,
+
+    price jsonb,                     -- { personType: price }
+
+    images text[],                   -- array of strings
+
+    -- location
+    zipcode text,
+    city text,
+    location text,
+    latitude numeric,
+    longitude numeric,
+
+    -- apartment details
+    guest_capacity int,
+    bathrooms_count int,
+    bedrooms_count int,
+    living_area int,                 -- sqft
+    is_parking_available boolean,
+
+    -- attraction
+    distance numeric,                -- km
+    accessibility_points text[],     -- array of strings
+    rules text[],                    -- array of strings
+
+    status text check (status in ('open', 'closed')),
+    features text[],                 -- array of strings
+
+    created_at timestamptz,
+    updated_at timestamptz
+);
+
